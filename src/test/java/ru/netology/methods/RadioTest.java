59a68f38d4e3b4723071e26307a3693e5bd2e8ff
Sona <s.harutyunyan1992@gmail.com>
2023-08-18 12:22:19 +0400

    Final version

--- src/test/java/ru/netology/methods/RadioTest.java
+++ src/test/java/ru/netology/methods/RadioTest.java
@@ -16,6 +16,54 @@ public class RadioTest {
 
     }
     @Test
+    public void shouldTestSetChannelUnderZero(){
+        Radio chan= new Radio();
+        chan.setChannel(-1);
+        int expected=0;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldSetChannelZero(){
+        Radio chan= new Radio();
+        chan.setChannel(0);
+        int expected=0;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldSetChannelOne(){
+        Radio chan= new Radio();
+        chan.setChannel(1);
+        int expected=1;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldSetChannelNine(){
+        Radio chan= new Radio();
+        chan.setChannel(9);
+        int expected=9;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldSetChannelEight(){
+        Radio chan= new Radio();
+        chan.setChannel(8);
+        int expected=8;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldSetChannelAboveNine(){
+        Radio chan= new Radio();
+        chan.setChannel(10);
+        int expected=9;
+        int actual= chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
     public void shouldTestNextChannel(){
         Radio chan= new Radio();
         chan.setChannel(8);
@@ -71,6 +119,70 @@ public class RadioTest {
         chan.prevChannel();
         int expected=0;
         int actual=chan.getChannel();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeUp(){
+        Radio vol= new Radio();
+        vol.setVolume(50);
+        vol.volumeUp();
+        int expected=51;
+        int actual=vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void shouldTestVolumeUpAboveHundred(){
+        Radio vol= new Radio();
+        vol.setVolume(100);
+        vol.volumeUp();
+        int expected=100;
+        int actual=vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeUpIfItsZero(){
+        Radio vol= new Radio();
+        vol.setVolume(0);
+        vol.volumeUp();
+        int expected=1;
+        int actual=vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeUpIfItsNinetyNine(){
+        Radio vol= new Radio();
+        vol.setVolume(99);
+        vol.volumeUp();
+        int expected=100;
+        int actual=vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeDown(){
+        Radio vol= new Radio();
+        vol.setVolume(100);
+        vol.volumeDown();
+        int expected=99;
+        int actual= vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeDownUnderZero(){
+        Radio vol= new Radio();
+        vol.setVolume(0);
+        vol.volumeDown();
+        int expected=0;
+        int actual= vol.getVolume();
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldTestVolumeDownIfItsOne(){
+        Radio vol= new Radio();
+        vol.setVolume(1);
+        vol.volumeDown();
+        int expected=0;
+        int actual= vol.getVolume();
         Assertions.assertEquals(expected,actual);
     }
 }
